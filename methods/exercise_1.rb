385901362ce7917bfc9a4f8ad16fd60bfe1affc4
Joel Harrington <thejoelharrington@gmail.com>
2021-04-15 20:43:14 -0500

    chapter 3 of intro to ruby

---
+++ methods/exercise_1.rb
@@ -0,0 +1,5 @@
+def greeting(name)
+  puts "hello #{name}"
+end
+
+puts greeting("Joel")
---
+++ methods/exercise_2.rb
@@ -0,0 +1,9 @@
+1. x = 2 #evaluates to 2
+
+2. puts x = 2 #evaluates to nil
+
+3. p name = "Joe" #evaluates to "Joe"
+
+4. four = "four" #evaluates to "four"
+
+5. print something = "nothing" #evaluates to nil
